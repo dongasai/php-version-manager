e0fad5dc58f1502e4ba309ee5ab86f1d775b5df4
dongasai <1514582970@qq.com>
2025-05-17 01:13:38 +0800

    feat(server): 添加镜像服务器管理功能
    - 实现了启动、停止、重启和查看服务器状态的功能
    - 新增 server 命令及其相关子命令
    - 添加服务器管理相关的帮助信息
    - 优化了代码结构，提高了可维护性

---
+++ srcMirror/Application.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Mirror;
+
+use Mirror\Command\CleanCommand;
+use Mirror\Command\CommandInterface;
+use Mirror\Command\HelpCommand;
+use Mirror\Command\ServerCommand;
+use Mirror\Command\StatusCommand;
+use Mirror\Command\SyncCommand;
+
+/**
+ * 应用程序类
+ */
+class Application
+{
+    /**
+     * 命令列表
+     *
+     * @var array
+     */
+    private $commands = [];
+
+    /**
+     * 构造函数
+     */
+    public function __construct()
+    {
+        // 注册命令
+        $this->registerCommands();
+    }
+
+    /**
+     * 注册命令
+     */
+    private function registerCommands()
+    {
+        $this->addCommand(new SyncCommand());
+        $this->addCommand(new StatusCommand());
+        $this->addCommand(new CleanCommand());
+        $this->addCommand(new ServerCommand());
+        $this->addCommand(new HelpCommand($this));
+    }
+
+    /**
+     * 添加命令
+     *
+     * @param CommandInterface $command 命令对象
+     */
+    public function addCommand(CommandInterface $command)
+    {
+        $this->commands[$command->getName()] = $command;
+    }
+
+    /**
+     * 获取命令
+     *
+     * @param string $name 命令名称
+     * @return CommandInterface|null
+     */
+    public function getCommand($name)
+    {
+        return isset($this->commands[$name]) ? $this->commands[$name] : null;
+    }
+
+    /**
+     * 获取所有命令
+     *
+     * @return array
+     */
+    public function getCommands()
+    {
+        return $this->commands;
+    }
+
+    /**
+     * 运行应用程序
+     *
+     * @param array $args 命令行参数
+     * @return int 退出代码
+     */
+    public function run(array $args = [])
+    {
+        // 如果没有参数，显示帮助信息
+        if (count($args) < 2) {
+            return $this->getCommand('help')->execute([]);
+        }
+
+        // 获取命令名称
+        $commandName = $args[1];
+
+        // 获取命令参数
+        $commandArgs = array_slice($args, 2);
+
+        // 获取命令
+        $command = $this->getCommand($commandName);
+
+        // 如果命令不存在，显示错误信息
+        if ($command === null) {
+            echo "未知命令: $commandName\n";
+            return $this->getCommand('help')->execute([]);
+        }
+
+        // 执行命令
+        return $command->execute($commandArgs);
+    }
+}
--- srcMirror/Autoloader.php
+++ srcMirror/Autoloader.php
@@ -11,15 +11,26 @@ class Autoloader
     public static function register()
     {
         spl_autoload_register(function ($class) {
+            // 检查是否是Mirror命名空间
+            $prefix = 'Mirror\\';
+            $len = strlen($prefix);
+
+            if (strncmp($prefix, $class, $len) !== 0) {
+                return false; // 不是我们的命名空间
+            }
+
+            // 获取相对类名
+            $relativeClass = substr($class, $len);
+
             // 将命名空间转换为文件路径
-            $file = ROOT_DIR . '/srcMirror/' . str_replace('\\', '/', str_replace('Mirror\\', '', $class)) . '.php';
-            
+            $file = ROOT_DIR . '/srcMirror/' . str_replace('\\', '/', $relativeClass) . '.php';
+
             // 如果文件存在，则包含它
             if (file_exists($file)) {
                 require $file;
                 return true;
             }
-            
+
             return false;
         });
     }
